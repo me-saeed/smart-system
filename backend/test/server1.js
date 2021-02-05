e955f407ee39f15a15f8b5c641b9d09a635be1d6
github67890 <62692034+github67890@users.noreply.github.com>
2021-02-05 17:36:07 +0500

    backebd

---
+++ backend/test/server1.js
@@ -0,0 +1,52 @@
+
+const express = require("express");
+const app = require('express')();
+const axios = require("axios");
+
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const router = express.Router();
+
+
+
+app.use(cors());
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
+app.use("/api", router);
+
+
+
+
+
+
+
+
+
+
+
+
+
+io.on("connection", (socket) => {
+  
+});
+
+
+
+
+
+module.exports = router;
+
+
+
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use('*', (req, res) => {
+console.log("Yes i have recieved request")
+
+});
+
+app.listen(process.env.PORT || 3000, () => console.log(`Running on PORT ${process.env.PORT || 5000}`));
