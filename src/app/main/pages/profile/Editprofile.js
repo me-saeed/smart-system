c8f519e176a43050e2e84c411bb04cb5a401adbd
Ibad Ur Rehman <ibadurrehman78@gmail.com>
2021-02-08 16:06:07 +0500

    update

--- src/app/main/pages/profile/Editprofile.js
+++ src/app/main/pages/profile/Editprofile.js
@@ -12,6 +12,24 @@ import TextField from '@material-ui/core/TextField';
 import { useForm } from "react-hook-form";
 import { connect } from "react-redux";
 
+import axios from 'axios';
+
+
+const apiUrl = 'http://localhost:5000';
+axios.interceptors.request.use(
+  config => {
+    const { origin } = new URL(config.url);
+    const allowedOrigins = [apiUrl];
+    const token = localStorage.getItem('token');
+    if (allowedOrigins.includes(origin)) {
+      config.headers.authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
 
 
 const useStyles = makeStyles((theme) => ({
@@ -52,6 +70,30 @@ function Editprofile() {
         ////SaveDetails();
       }
     }
+
+
+
+    const storedJwt = localStorage.getItem('token');
+    const [jwt, setJwt] = useState(storedJwt || null);
+    const [foods, setFoods] = useState([]);
+    const [fetchError, setFetchError] = useState(null);
+  const getJwt = async () => {
+    alert(jwt)
+      const { data } = await axios.get(`${apiUrl}/jwt`);
+      localStorage.setItem('token', data.token);
+      setJwt(data.token);
+    };
+  const getFoods = async () => {
+      try {
+        const { data } = await axios.get(`${apiUrl}/foods`);
+        setFoods(data);
+        setFetchError(null);
+      } catch (err) {
+        setFetchError(err.message);
+      }
+    };
+
+
 
     return (
         <div>
@@ -150,6 +192,33 @@ function Editprofile() {
       
     
     </form>
+
+
+<br/><br/>
+
+{/* jwt token */}
+<section style={{ marginBottom: '10px' }}>
+        <button onClick={() => getJwt()}>Get JWT</button>
+        {jwt && (
+          <pre>
+            <code>{jwt}</code>
+          </pre>
+        )}
+      </section>
+      <section>
+        <button onClick={() => getFoods()}>
+          Get Foods
+        </button>
+        <ul>
+          {foods.map((food, i) => (
+            <li>{food.description}</li>
+          ))}
+        </ul>
+        {fetchError && (
+          <p style={{ color: 'red' }}>{fetchError}</p>
+        )}
+      </section>
+
 
 
                  </div>
