29b5e985c528fe95d45732af7fed8efd4c567e51
github67890 <62692034+github67890@users.noreply.github.com>
2021-02-11 23:04:24 +0500

    Revert "xad"
    This reverts commit defaaeeeb747271618090659737b3d6f6bdfe4ff.

--- src/app/main/apps/appsConfigs.js
+++ src/app/main/apps/appsConfigs.js
@@ -10,7 +10,7 @@ import MailAppConfig from './mail/MailAppConfig';
 import NotesAppConfig from './notes/NotesAppConfig';
 import ScrumboardAppConfig from './scrumboard/ScrumboardAppConfig';
 import TodoAppConfig from './todo/TodoAppConfig';
-import AdminConfig from './Admin/Adminconfig';
+
 const appsConfigs = [
 	AnalyticsDashboardAppConfig,
 	ProjectDashboardAppConfig,
@@ -23,8 +23,7 @@ const appsConfigs = [
 	ECommerceAppConfig,
 	ScrumboardAppConfig,
 	AcademyAppConfig,
-	NotesAppConfig,
-	AdminConfig
+	NotesAppConfig
 ];
 
 export default appsConfigs;
