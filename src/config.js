009034e8b2246b9fafaa72336ea827abff84cb7e
Ibad Ur Rehman <56487132+me-ibad@users.noreply.github.com>
2021-02-19 00:07:02 +0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,16 +1,12 @@
-// module.exports = {
 
-//  servername: "http://10.0.2.2:5000",
-//   ////servername: "http://192.168.43.54:5000",
-// };
 module.exports = {
 
-///servername: "https://latestlocaldealz.com/api",
- 
+
+
  servername:"http://localhost:5000"
 
 
  
-/////servername: "http://18.205.163.46:5000",
+
 
 };
